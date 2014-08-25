bcefca81a09d7bafe318c9ca68e93090dc8cef5b
Joe Fleming <joe.fleming@gmail.com>
2014-08-25 10:42:40 -0700

    initial commit; working ES setup

---
+++ Vagrantfile
@@ -0,0 +1,11 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "precise64"
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.provision :shell, :path => "provision.sh"
+  config.vm.network :forwarded_port, host: 9200, guest: 9200
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.customize ["modifyvm", :id, "--cpuexecutioncap", "60", "--cpus", "2"]
+    vb.memory = 2048
+  end
+end
---
+++ provision.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# User settings
+ESVERSION='1.2.2'
+ESURL="https://download.elasticsearch.org/elasticsearch/elasticsearch"
+TARFILE="elasticsearch-${ESVERSION}.tar.gz"
+
+# Allow installation of PPAs and update packages
+apt-get update
+apt-get install -y python-software-properties
+
+# Automated install of Oracle Java
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get install -y oracle-java7-installer
+
+# Grab and unpack ElasticSearch tarball
+wget -q "${ESURL}/${TARFILE}"
+su vagrant -c "tar -zxf ${TARFILE}"
+
+echo "ElasticSearch is not running, SSH in to start it"
